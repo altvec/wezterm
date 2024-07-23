cb89b233a02da2d2ca3e5065d1db5be3bb360b55
altvec <altvec@gmail.com>
2024-07-23 16:51:19 +0500

    fixes

--- wezterm.lua
+++ wezterm.lua
@@ -10,12 +10,15 @@ end
 -- Settings
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font_with_fallback({
-	{ family = "VictorMono Nerd Font", scale = 1.13 },
-	{ family = "VictorMono Nerd Font Mono", scale = 1.13 },
-	{ family = "Fira Code Retina", scale = 1.13 },
+	{ family = "VictorMono Nerd Font", scale = 1.15 },
+	{ family = "VictorMono Nerd Font Mono", scale = 1.15 },
+	{ family = "Fira Code Retina", scale = 1.15 },
 })
-config.window_background_opacity = 0.97
-config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.90
+config.macos_window_background_blur = 10
+config.window_decorations = "TITLE | RESIZE"
+config.enable_tab_bar = true
+
 config.scrollback_lines = 30000
 
 return config
